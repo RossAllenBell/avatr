6e9cb3be47375477078555179f9d3fcca925c61b
Ross Bell <rbell@backstopsolutions.com>
2012-08-04 17:58:34 -0500

    Enable properties file usage

--- src/com/rossallenbell/avatr/AvatrServer.java
+++ src/com/rossallenbell/avatr/AvatrServer.java
@@ -1,16 +1,22 @@
 package com.rossallenbell.avatr;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.webapp.WebAppContext;
 
 public class AvatrServer {
+	
+	public final static Properties PROPERTIES = new Properties();
 
 	public static void main(String[] args) throws Exception {
+		
+		PROPERTIES.load(new FileInputStream("etc/server.properties"));
 
-		Server server = new Server(8080);
+		Server server = new Server(Integer.parseInt(PROPERTIES.getProperty("port", "8080")));
 
 		ServletContextHandler handler = new ServletContextHandler();
 		handler.setResourceBase("src/com/rossallenbell/avatr/webapp");
