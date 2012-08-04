475b73a0d69eebbc432cc56444de594acdbbed19
Ross Bell <rbell@backstopsolutions.com>
2012-08-04 16:55:37 -0500

    Added a build script to enable packaging/running as a jar. Webapp resources are packaged directly into the jar.

--- src/com/rossallenbell/avatr/AvatrServer.java
+++ src/com/rossallenbell/avatr/AvatrServer.java
@@ -4,26 +4,28 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class AvatrServer {
-	
-	public static void main(String[] args) throws Exception {	    
-        Server server = new Server(8080);
-        
-        ServletContextHandler handler = new ServletContextHandler();
-        handler.setResourceBase("webapp/");
-        
-        DefaultServlet defaultServlet = new DefaultServlet();
-        ServletHolder defaultServletHolder = new ServletHolder(defaultServlet);
-        defaultServletHolder.setInitParameter("useFileMappedBuffer", "false");
-        
-        handler.addServlet(defaultServletHolder, "/");        
-        handler.addServlet(new ServletHolder(new AvatrWebSocketServlet()), "/ws");
 
-        server.setHandler(handler);
-        
-        server.start();
-        server.join();        
-    }
+	public static void main(String[] args) throws Exception {
+
+		Server server = new Server(8080);
+
+		ServletContextHandler handler = new ServletContextHandler();
+		handler.setResourceBase("src/com/rossallenbell/avatr/webapp");
+
+		DefaultServlet defaultServlet = new DefaultServlet();
+		ServletHolder defaultServletHolder = new ServletHolder(defaultServlet);
+		defaultServletHolder.setInitParameter("useFileMappedBuffer", "false");
+
+		handler.addServlet(defaultServletHolder, "/");
+		handler.addServlet(new ServletHolder(new AvatrWebSocketServlet()), "/ws");
+
+		server.setHandler(handler);
+
+		server.start();
+		server.join();
+	}
 
 }
